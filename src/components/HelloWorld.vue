a3d6c4a2a692a1d248293546af47349b049e81ce
anton-mladenov <tonkata@mail.com>
2018-08-09 23:47:29 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+	<h3>{{ msg2 }}</h3>
+	<h6> {{ msg3 }} </h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Anton says Hi',
+	  msg2: "some real Vue JS testing is going on around here",
+	  msg3: "hahahah just kidding!"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
